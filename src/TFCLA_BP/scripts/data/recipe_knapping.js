5800c5d555ff7b22e420017c58b05a8068f34b62
32ByteMemory <sin24steel@gmail.com>
2026-06-30 12:09:51 +0900

    Feature Knapping Recipe

---
+++ src/TFCLA_BP/scripts/data/recipe_knapping.js
@@ -0,0 +1,46 @@
+// @ts-check
+
+/**
+ *
+ * @param { String[] } pattern
+ * @returns { Boolean[] }
+ */
+function convertPattern(pattern) {
+    if (pattern.length !== 5)
+        throw new Error("Pattern must have 5 rows.");
+
+    if (pattern.some(row => row.length !== 5))
+        throw new Error("Each row must have 5 columns.");
+
+    return pattern.flatMap(row => row.split("").map(char => char === "1"));
+};
+
+/**
+ *
+ * @param { String } material
+ * @param { String } output
+ * @param { String[] } pattern
+ */
+function createKnappingRecipe(material, output, pattern) {
+    return {
+        material,
+        output,
+        pattern: convertPattern(pattern)
+    };
+};
+
+export const KnappingRecipes = [
+
+    createKnappingRecipe(
+        "tfcla:stone_loose_rock",
+        "tfcla:stone_axe_head",
+        [
+            "01000",
+            "11110",
+            "11111",
+            "11110",
+            "01000"
+        ]
+    )
+
+];
